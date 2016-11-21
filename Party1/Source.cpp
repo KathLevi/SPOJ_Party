53fe5e2bf2ffdf158f72d1f0e0e10ca01d6a2347
KathLevi <levi.kathleen1@gmail.com>
2016-11-21 10:33:06 -0800

    edited the algorithm and rearranged the main function

--- Party1/Source.cpp
+++ Party1/Source.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-int fee[101];			//array of the fee for party[i]
-int fun[101];			//array of the fun for party[i]
 
-void maxFun(int budget, int n) {
+void maxFun(int budget, int n, vector<int> &sumFun, vector<int> &sumFee, int *fun, int *fee) {
 	int w, k, total = 0;
 	int B[502];
 
@@ -21,31 +20,57 @@ void maxFun(int budget, int n) {
 	int b = budget; 
 	while (B[b] == B[budget])
 		b--;
-	cout << b + 1 <<" " << B[b + 1] << endl;
+	sumFun.push_back(B[b + 1]);
+	sumFee.push_back(b + 1);
+}
+
+int getFirstLine(int &budget, int &n, vector<int> &sumFun, vector<int> &sumFee) {
+	//first line to read in budget and n
+	cin >> budget;		//party budget (0-500)
+	cin >> n;			//number n of parties (0-100)
+
+	if (budget == 0 && n == 0)
+		return 3;
+	else if (budget == 0 || n == 0) {
+		sumFun.push_back(0);
+		sumFee.push_back(0);
+		return -1;
+	}
+	else {
+		return 1;
+	}
+		
 }
 
 int main() {
+	int fee[101];			//array of the fee for party[i]
+	int fun[101];			//array of the fun for party[i]
+
+	vector<int> sumFun;
+	vector<int> sumFee;
+
 	int budget = -1;
 	int n = -1;			//budget, n (the number of parties)
-	
-	do {
-		//first line to read in budget and n
-		cin >> budget;		//party budget (0-500)
-		cin >> n;			//number n of parties (0-100)
+	int value;
 
-		if (budget == 0 && n == 0)
-			return 0;
-	
-		if (budget == 0 || n == 0)
-			cout << "0 0" << endl;
-		else {
+	do {
+		value = getFirstLine(budget, n, sumFun, sumFee);
+		if (value < 2) {
 			for (int i = 0; i < n; i++) {
 				cin >> fee[i];		//entrance fee of each party (5-25 francs)
 				cin >> fun[i];		//amount of fun per party (0- 10)
 			}
-			maxFun(budget, n);		//determine the max fun without going over budget
+			if (value == 1) {
+				maxFun(budget, n, sumFun, sumFee, fun, fee);		//determine the max fun without going over budget
+			}
 		}
 	} while (budget != 0 && n != 0);
+
+	for (unsigned int i = 0; i < sumFee.size(); i++)
+		cout << sumFee[i] << " " << sumFun[i] << endl;
+
+	sumFee.clear();
+	sumFun.clear();
 
 	return 0;
 }
